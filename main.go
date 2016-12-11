b4c7ca7d0f906c1696a50cf8b83c61d796208628
root <root@ubuntu-xenial.localdomain>
2016-12-11 19:26:39 +0000

    Main should run command passed in

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
 
 func main() {
-	fmt.Println("main")
+	cmd := exec.Command(os.Args[1], os.Args[2:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("ERROR " + err.Error())
+		os.Exit(1)
+	}
 }
