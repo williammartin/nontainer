86bab992a1cc7a58f1b7dc4c748f5e81ed9f2fac
CF Garden <garden@pivotal.io>
2016-12-12 19:53:06 +0000

    Mount proc back in container

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	case "child":
 		child()
 	default:
-		panic("ughh")
+		panic("dufhdsag")
 	}
 }
 
@@ -26,16 +26,19 @@ func parent() {
 	cmd.SysProcAttr = &syscall.SysProcAttr{Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS}
 
 	if err := cmd.Run(); err != nil {
-		fmt.Println("ERROR " + err.Error())
+		fmt.Println("ERROR", err)
 		os.Exit(1)
 	}
 }
 
 func child() {
-
 	must(syscall.Mount("rootfs", "rootfs", "", syscall.MS_BIND, ""))
 	must(os.MkdirAll("rootfs/oldrootfs", 0700))
 	must(syscall.PivotRoot("rootfs", "rootfs/oldrootfs"))
+	must(os.Chdir("/"))
+
+	must(syscall.Unmount("oldrootfs", syscall.MNT_DETACH))
+	must(syscall.Mount("proc", "/proc", "proc", 0, ""))
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
@@ -43,7 +46,7 @@ func child() {
 	cmd.Stderr = os.Stderr
 
 	if err := cmd.Run(); err != nil {
-		fmt.Println("ERROR " + err.Error())
+		fmt.Println("ERROR", err)
 		os.Exit(1)
 	}
 }
