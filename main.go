2a3c4da65c70c31204bd82c8fba4315803761440
Will Martin <wmartin@pivotal.io>
2016-12-12 17:37:42 +0000

    Add correct rootfs and pivot in

--- main.go
+++ main.go
@@ -8,7 +8,18 @@ import (
 )
 
 func main() {
-	cmd := exec.Command(os.Args[1], os.Args[2:]...)
+	switch os.Args[1] {
+	case "run":
+		parent()
+	case "child":
+		child()
+	default:
+		panic("ughh")
+	}
+}
+
+func parent() {
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -17,5 +28,28 @@ func main() {
 	if err := cmd.Run(); err != nil {
 		fmt.Println("ERROR " + err.Error())
 		os.Exit(1)
+	}
+}
+
+func child() {
+
+	must(syscall.Mount("rootfs", "rootfs", "", syscall.MS_BIND, ""))
+	must(os.MkdirAll("rootfs/oldrootfs", 0700))
+	must(syscall.PivotRoot("rootfs", "rootfs/oldrootfs"))
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("ERROR " + err.Error())
+		os.Exit(1)
+	}
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
 	}
 }
